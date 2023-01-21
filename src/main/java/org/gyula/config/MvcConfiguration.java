7951a6c1347dfca750b65c2b50dd2b834335c4a7
Gyula Szabo <44570623+gyuvi02@users.noreply.github.com>
2023-01-21 14:22:46 +0100

    list-customers.jsp added

--- src/main/java/org/gyula/config/MvcConfiguration.java
+++ src/main/java/org/gyula/config/MvcConfiguration.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages="org.gyula")
 @EnableWebMvc
-public class MvcConfiguration extends WebMvcConfigurerAdapter{
+public class MvcConfiguration implements WebMvcConfigurer {
 
 	@Bean
 	public ViewResolver getViewResolver(){
@@ -27,5 +25,12 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
-	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+//        WebMvcConfigurer.super.addViewControllers(registry);
+	}
+
+
+
 }
--- src/main/java/org/gyula/controller/CustomerController.java
+++ src/main/java/org/gyula/controller/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/customer")
@@ -13,7 +14,7 @@ public class CustomerController {
     @Autowired
     private CustomerDAO customerDAO;
 
-    @RequestMapping("/list")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String listCustomers(Model model) throws Exception {
         model.addAttribute(customerDAO.getCustomers());
 
